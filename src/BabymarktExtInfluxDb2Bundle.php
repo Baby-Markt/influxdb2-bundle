e2ae813b77fe4bfc9d0ee5428aea7a087b6e0450
Niklas Funke <funke@babymarkt.de>
2021-12-26 17:07:37 +0100

    Initial commit

---
+++ src/BabymarktExtInfluxDb2Bundle.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Copyright (c) 2015 Babymarkt.de GmbH - All Rights Reserved
+ *
+ * All information contained herein is, and remains the property of Baby-Markt.de
+ * and is protected by copyright law. Unauthorized copying of this file or any parts,
+ * via any medium is strictly prohibited.
+ */
+
+namespace BabymarktExt\InfluxDb2Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BabymarktExtInfluxDb2Bundle extends Bundle
+{
+}
---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2015 Babymarkt.de GmbH - All Rights Reserved
+ *
+ * All information contained herein is, and remains the property of Baby-Markt.de
+ * and is protected by copyright law. Unauthorized copying of this file or any parts,
+ * via any medium is strictly prohibited.
+ */
+
+namespace BabymarktExt\InfluxDb2Bundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('babymarkt_ext_influxdb2');
+        $rootNode = $treeBuilder->getRootNode();
+
+
+
+        return $treeBuilder;
+    }
+}
